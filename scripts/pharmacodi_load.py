6f57d051ece9c3c6e40dcf4364ff6af5d5853376
ChristopherEeles <christopher.eeles@outlook.com>
2021-08-21 03:19:19 +0000

    Fix: chunking for batch insertion works again

--- scripts/pharmacodi_load.py
+++ scripts/pharmacodi_load.py
@@ -436,8 +436,8 @@ def bulk_chunk_insert(file_path, table, chunksize=100000):
     session = Session(bind=engine)
     data_df = dt.fread(file_path)
     nchunks = math.floor(data_df.shape[0] / 100000)
-    chunk_array = np.split(np.arange(data_df.shape[0]), nchunks)
-    index_tuple_list = [(np.min(x), np.max(x)) for x in chunk_array]
+    chunk_array = np.array_split(np.arange(data_df.shape[0]), nchunks)
+    index_tuple_list = [(int(np.min(x)), int(np.max(x))) for x in chunk_array]
     for idx in tqdm(index_tuple_list, colour='magenta'):
         df = data_df[idx[0]:idx[1], :].to_pandas()
         row_dict = create_records(df)
